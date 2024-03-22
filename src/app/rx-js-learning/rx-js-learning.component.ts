650f7c1a2614e8de6fd3e64df8c4dec247fe4aca
Alireza <alireza1999haghshenas@gmail.com>
2024-03-22 18:40:38 +0330

    update

--- src/app/rx-js-learning/rx-js-learning.component.ts
+++ src/app/rx-js-learning/rx-js-learning.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-rx-js-learning',
   templateUrl: './rx-js-learning.component.html',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 export class RxJsLearningComponent {
 
-  
+  observable = new Observable((observer) => {
+    observer.next('hello');
+    observer.next('world');
+    setInterval(() =>{
+      observer.next('hello world');
+    },2000);
+  });
+  ngOnInit(): void{
+    const observerOne = this.observable.subscribe(
+      (value) => this.addItem(value),
+      (error) => console.log('error'),
+      () => console.log('complete')
+    );
+    setTimeout(()=> {
+      observerOne.unsubscribe();
+    },6001);
+  };
+  addItem(value:any){
+    console.log(value);
+  }
 }
