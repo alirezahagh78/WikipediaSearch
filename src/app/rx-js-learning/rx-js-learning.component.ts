0d34af6e24a42d454b88accd6db9bf63d3cb82c9
Alireza <alireza1999haghshenas@gmail.com>
2024-03-22 18:31:20 +0330

    first commit

---
+++ src/app/rx-js-learning/rx-js-learning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rx-js-learning',
+  templateUrl: './rx-js-learning.component.html',
+  styleUrls: ['./rx-js-learning.component.css']
+})
+export class RxJsLearningComponent {
+
+  
+}
