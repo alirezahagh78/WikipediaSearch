ede53a42aefde34b83f6838e17dbbabe369c4c28
Alireza <alireza1999haghshenas@gmail.com>
2024-03-24 11:41:41 +0330

    update operators

--- src/app/operators/operators.component.ts
+++ src/app/operators/operators.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { from, Observable, Subject } from 'rxjs';
+import { map, pluck, skipUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-operators',
@@ -12,32 +12,48 @@ export class OperatorsComponent {
 
   constructor() { }
 
+  // ngOnInit(): void {
+  //   const observable = new Observable<string>((observer) => {
+  //     observer.next('hi')
+  //   }).pipe(
+  //     map(val => val.toUpperCase()),
+  //   );
+  //   const observer = observable.subscribe((data) => {
+  //     console.log(data);
+  //   });
+
+  //   from([{
+  //     name: 'ali',
+  //     age: 28,
+  //   },
+  //   {
+  //     name: 'reza',
+  //     age: 29,
+  //   },
+  //   {
+  //     name: 'milad',
+  //     age: 30,
+  //   }
+  //   ]).pipe(
+  //     pluck('age')
+  //   ).subscribe((data) => {
+  //     console.log(data);
+  //   });
+  // }
   ngOnInit(): void {
-    const observable = new Observable<string>((observer) => {
-      observer.next('hi')
-    }).pipe(
-      map(val => val.toUpperCase()),
-    );
-    const observer = observable.subscribe((data) => {
-      console.log(data);
+    const observableOne = new Observable((observer) => {
+    let i = 1;
+    setInterval(()=>{
+      observer.next(i++);
+    },1000)
     });
 
-    from([{
-      name: 'ali',
-      age: 28,
-    },
-    {
-      name: 'reza',
-      age: 29,
-    },
-    {
-      name: 'milad',
-      age: 30,
-    }
-    ]).pipe(
-      pluck('age')
-    ).subscribe((data) => {
-      console.log(data);
-    });
+    const observableTwo = new Subject();
+    setTimeout(()=>{
+      observableTwo.next('hi')
+    },3000);
+    const newObs = observableOne.pipe(skipUntil(observableTwo));
+    newObs.subscribe(x => console.log(x)
+    );
   }
 }
